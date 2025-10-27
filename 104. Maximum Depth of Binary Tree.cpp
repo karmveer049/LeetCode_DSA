7672542313d1629860bab8d3565d4631a81aa9df
Karmveer <23bee050@nith.ac.in>
2025-10-28 00:04:22 +0530

    Create 104. Maximum Depth of Binary Tree.cpp

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,43 @@
+//PROBLEM DESCRIPTION
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+
+//CPP CODE HERE
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        if(!root) return 0;
+        int depth= 0;
+        while(!q.empty()){
+            int level=q.size();
+            for(int i=0;i<level;i++){
+                TreeNode* curr=q.front();
+                q.pop();
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            depth++;
+        }
+        return depth;
+        
+    }
